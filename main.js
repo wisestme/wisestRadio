d2813790d3b15b7ee0bfb8bfb261dd7b723303d1
wisestme01 <wyhzest@gmail.com>
2022-02-02 09:09:59 +0100

    add songs objects

--- main.js
+++ main.js
@@ -6,11 +6,33 @@ const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 const playBtn = document.getElementById('play');
 
-// const songs = [
-//   {
-//     name: 'phil'
-//   }
-// ]
+const songs = [
+  {
+    name: 'phil_1',
+    displayName: 'Anthem',
+    artist: 'Phil Wickham'
+  },
+  {
+    name: 'phil_2',
+    displayName: 'Hold On',
+    artist: 'Phil Wickham'
+  },
+  {
+    name: 'phil_3',
+    displayName: 'Lord I need you',
+    artist: 'Phil Wickham'
+  },
+  {
+    name: 'phil_4',
+    displayName: 'Secret Place',
+    artist: 'Phil Wickham'
+  },
+  {
+    name: 'phil_5',
+    displayName: 'Wide Awake',
+    artist: 'Phil Wickham'
+  }
+]
 
 // Check playing
 let isPlaying = false;
